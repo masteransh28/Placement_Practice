15393e25195ba1ba5b2ef4534f1543399fa80fdf
masteransh28 <thepriyanshusingh28@gmail.com>
2026-09-10 16:26:51 +0530

    Time: 0 ms (100%), Space: 44.1 MB (15.19%) - LeetHub - Time: O(n), Space: O(h)

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int moves = 0;
+    public int distributeCoins(TreeNode root){
+        dfs(root);
+        return moves;
+    }
+    private int dfs(TreeNode node){
+        if(node == null)return 0;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        moves+=Math.abs(left)+Math.abs(right);
+        return node.val+left+right-1;
+    }
+}
+
+       
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
